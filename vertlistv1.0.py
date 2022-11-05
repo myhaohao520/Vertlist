2048c67f7480d0385ab43f984ca6e55a3555e568
myhaohao520 <50754881+myhaohao520@users.noreply.github.com>
2022-11-04 22:18:07 -0700

    Add files via upload

---
+++ vertlistv1.0.py
@@ -0,0 +1,14 @@
+#Python Script for printing list of names verticially (Replace the names in Step 2 with your own) V1.0
+
+#Step 1. The list of names I will be using in this example
+#list = Carlo Pineda, Eduardo Covarrubias, Ting-Yueh Liu, Matt Dulog, Jian Hao Tang, Son Tran
+
+#Step 2. Put the list on names in '', run the code to split all the names into individual ''
+list = 'Carlo Pineda, Eduardo Covarrubias, Ting-Yueh Liu, Matt Dulog, Jian Hao Tang, Son Tran'
+print(list.split(','))
+
+#Step 3. Copy the output of the previous code and put the list into []
+list = ['Carlo Pineda', ' Eduardo Covarrubias', ' Ting-Yueh Liu', ' Matt Dulog', ' Jian Hao Tang', ' Son Tran']
+
+#Step 4. Run this code to print the list of names vertically
+print('\n'.join(list))
